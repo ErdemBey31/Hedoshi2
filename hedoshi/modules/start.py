cc6f99a5dbe833128fa2ff1e5ea75a002a4339de
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-08-23 11:43:54 +0300

    Update start.py

--- hedoshi/modules/start.py
+++ hedoshi/modules/start.py
@@ -1,3 +1,16 @@
+# Copyright (C) 2023 frknkrc44 <https://gitlab.com/frknkrc44>
+#
+# This file is part of HedoshiMusicBot project,
+# and licensed under GNU Affero General Public License v3.
+# See the GNU Affero General Public License for more details.
+#
+# All rights reserved. See COPYING, AUTHORS.
+#
+
+from pyrogram.types import Message
+from ..helpers.telegram.cmd_register import register
+from time import time
+
 @register('start', private=True)
 async def start(message: Message):
     buttons = [[
