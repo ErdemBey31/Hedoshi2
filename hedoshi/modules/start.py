500df8995130da288b30c69456ea8fff50b2aaa0
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-08-23 10:32:47 +0300

    Add files via upload

--- hedoshi/modules/start.py
+++ hedoshi/modules/start.py
@@ -1,35 +1,39 @@
-# Copyright (C) 2023 frknkrc44 <https://gitlab.com/frknkrc44>
-#
-# This file is part of HedoshiMusicBot project,
-# and licensed under GNU Affero General Public License v3.
-# See the GNU Affero General Public License for more details.
-#
-# All rights reserved. See COPYING, AUTHORS.
-#
+@register('start', private=True)
+async def start(message: Message):
 
-from pyrogram.types import Message
-from ..helpers.telegram.cmd_register import register
-from time import time
+```
+buttons = [[
+    InlineKeyboardButton("Komutları Göster", callback_data="show_commands") 
+],
+]
 
+await message.reply_text(
+    "Merhaba, Müzik Bot'a hoş geldiniz!",
+    reply_markup=InlineKeyboardMarkup(buttons)
+)
+```
 
-@register('start|baslat|alive', private=True)
-async def ping(message: Message) -> None:    
-    await message.reply_text('''__Merhaba, Türkiye Müzik Bot'a hoşgeldin,__
+@callback_query_handler(func=lambda call: call.data == "show_commands")
+async def show_commands(call: CallbackQuery):
+
+```
+await call.message.edit_text(    """
+    Komutlar:
     
-   ** KOMUTLAR:**
+    /play - Müzik oynat
+    /vplay - Video oynat
+    ...
     
-    **/play - Müzik oynatır, bir dosyaya yanıt veya bir müzik adı**
-    **/vplay - Videolu oynat **
-   ** /end - Bitir**
-   ** /skip - Sonraki müziğe geç**
-   ** /seek - Buraya yazılan saniye kadar ileri sarar**
-   ** /ping - pingi ölçmek**
-   ** /pause - durdur**
-  **  /resume - devam et**
-  **  /loop - döngü**
-   ** /leave - sesliden ayrıl**
-   ** /query - Oynatılan müziği göster(Not: ismin sonunda ki -a şarkı -v video demektir)**
+    Geri dönmek için 'Geri' butonuna basın
+    """,
+    reply_markup=InlineKeyboardMarkup([[
+        InlineKeyboardButton("Geri", callback_data="back_to_start") 
+    ]])
+```
 
-   ** GÜNCELLEME KANALINA KATIL @turkiyemuzikguncelleme**
-''')
-    
+)
+
+@callback_query_handler(func=lambda call: call.data == "back_to_start")\
+async def back_to_start(call: CallbackQuery):
+
+await start(call.message)
