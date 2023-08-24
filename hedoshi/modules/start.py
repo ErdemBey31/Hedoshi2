6e50ec9926e023366f8dc5c76aeea2e72bb6bdec
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-08-24 20:36:25 +0300

    Update start.py

--- hedoshi/modules/start.py
+++ hedoshi/modules/start.py
@@ -5,49 +5,23 @@
 # See the GNU Affero General Public License for more details.
 #
 # All rights reserved. See COPYING, AUTHORS.
-from pyrogram import Client, CallbackQuery
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram import filters
-from time import time
+
 from ..helpers.telegram.cmd_register import register
 
 @register('start', private=True)
 async def start(message: Message):
-    buttons = [
-        [InlineKeyboardButton("KOMUTLAR ✨", callback_data="show_commands")]
-    ]
-
-    await message.reply_text(
-        "__Merhaba, Müzik Botuna hoş geldiniz! Komutlarım aşağıdaki butondan görülebilir.__",
-        reply_markup=InlineKeyboardMarkup(buttons)
-    )
-
-
-@Client.on_callback_query(filters.regex("show_commands"))
-async def show_commands(_, query: CallbackQuery):
-    await query.edit_message_text(
-        """
+    message.reply_photo("https://telegra.ph/file/dd3b29610f7d702500f9d.jpg", caption="""
         ** KOMUTLAR:**
 
         ** /play - Müzik oynatır, bir dosyaya yanıt veya bir müzik adı**
         ** /vplay - Videolu oynat **
         ** /end - Bitir**
         ** /skip - Sonraki müziğe geç**
-        ** /seek - Buraya yazılan saniye kadar ileri sarar**
+        ** /seek-[ileri,geri] - Buraya yazılan saniye kadar ileri sarar**
         ** /ping - pingi ölçmek**
         ** /pause - durdur**
         ** /resume - devam et**
         ** /loop - döngü**
         ** /leave - sesliden ayrıl**
         ** /query - Oynatılan müziği göster(Not: ismin sonunda ki -a şarkı -v video demektir)**
-        ** GÜNCELLEME KANALINA KATIL @ruyamuzikguncelleme**
-        """,
-        reply_markup=InlineKeyboardMarkup([[
-            InlineKeyboardButton("Geri 🔙", callback_data="back_to_start")
-        ]])
-    )
-
-
-@Client.on_callback_query(filters.regex("back_to_start"))
-async def back_to_start(_, query: CallbackQuery):
-    await start(query.message)
+        ** GÜNCELLEME KANALINA KATIL @onlinemzkupdates**""")
